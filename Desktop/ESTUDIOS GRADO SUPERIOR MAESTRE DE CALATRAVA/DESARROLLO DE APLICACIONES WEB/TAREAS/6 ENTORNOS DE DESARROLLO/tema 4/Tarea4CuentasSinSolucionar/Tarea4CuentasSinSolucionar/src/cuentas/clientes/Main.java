b5347e2a41ff3a6ed965ba83fd169c723571db4f
anton <anton@Tayo>
2023-02-16 08:39:02 +0100

    Esta es la creacion de la variable Antonio

--- Desktop/ESTUDIOS GRADO SUPERIOR MAESTRE DE CALATRAVA/DESARROLLO DE APLICACIONES WEB/TAREAS/6 ENTORNOS DE DESARROLLO/tema 4/Tarea4CuentasSinSolucionar/Tarea4CuentasSinSolucionar/src/cuentas/clientes/Main.java
+++ Desktop/ESTUDIOS GRADO SUPERIOR MAESTRE DE CALATRAVA/DESARROLLO DE APLICACIONES WEB/TAREAS/6 ENTORNOS DE DESARROLLO/tema 4/Tarea4CuentasSinSolucionar/Tarea4CuentasSinSolucionar/src/cuentas/clientes/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Cuenta cuenta;
         double saldoActual;
+        String antonio;
 
         operativa_cuenta(1.2F);
     }
